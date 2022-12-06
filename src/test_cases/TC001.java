af4baab9cd495a9639280ea3c85b28fc90a585f3
Furkan Ervan <furkanervan@hotmail.de>
2022-12-06 15:57:04 +0100

    updated tc001

--- src/test_cases/TC001.java
+++ src/test_cases/TC001.java
@@ -4,12 +4,11 @@ public class TC001 {
     public static void main(String[] args) {
 
 
-
-
-
         // testing some things
 
         // more testing also main added
+
+        // more testing
 
     }
 
